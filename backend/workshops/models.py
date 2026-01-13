fb3b21e29dcf6aa6cb9cac47803a332d79bf82e9
BenjhyEA <benjhy1504@gmail.com>
2026-01-13 00:12:55 -0500

    feat(backend): crear modelos categoria y workshop

--- backend/workshops/models.py
+++ backend/workshops/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Workshop(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_date = models.DateTimeField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="workshops"
+    )
+    
+    def __str__(self):
+        return self.name
